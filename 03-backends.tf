e07706bff8a02f36ce6bd76c3b10ccdb3cacef47
1cepeak <foxwel.info@gmail.com>
2025-07-06 23:33:28 +0300

    Fix format

--- 03-backends.tf
+++ 03-backends.tf
@@ -3,13 +3,13 @@ terraform {
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_requesting_account_id  = true
-    endpoints                   = {
+    endpoints = {
       s3 = "http://77.37.140.142:7480"
     }
-    region                      = "eu-ru"
-    bucket                      = "terraform"
-    key                         = "state/terraform.tfstate"
-    encrypt                     = true
-    use_path_style              = true
+    region         = "eu-ru"
+    bucket         = "terraform"
+    key            = "state/terraform.tfstate"
+    encrypt        = true
+    use_path_style = true
   }
 }
