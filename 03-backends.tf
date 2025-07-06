5fe083bee64927132749b5733ae4117d12ba97fc
1cepeak <foxwel.info@gmail.com>
2025-07-06 23:01:26 +0300

    Add custom S3 backend settings

--- 03-backends.tf
+++ 03-backends.tf
@@ -1,26 +1,12 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "terraform-state-bucket"
-#     key            = "terraform.tfstate"
-#     region         = "us-east-1"
-#     encrypt        = true
-#     dynamodb_table = "terraform-state-lock"
-#   }
-# }
-
-# resource "aws_s3_bucket" "terraform_state" {
-#   bucket = "terraform-state-bucket"
-# }
-
-# resource "aws_dynamodb_table" "terraform_state_lock" {
-#   name = "terraform-state-lock"
-#   hash_key = "LockID"
-#   attribute {
-#     name = "LockID"
-#     type = "S"
-#   }
-#   server_side_encryption {
-#     enabled = true
-#   }
-#   lifecycle {
-# }
+terraform {
+  backend "s3" {
+    endpoint                    = "http://77.37.140.142:7480"
+    bucket                      = "terraform"
+    key                         = "state/terraform.tfstate"
+    encrypt                     = true
+    access_key                  = var.S3_ACCESS_KEY
+    secret_key                  = var.S3_SECRET_KEY
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
