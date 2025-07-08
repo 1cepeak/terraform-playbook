df2a9d6f597b50ce58db93422b421b42dca91297
1cepeak <foxwel.info@gmail.com>
2025-07-08 04:15:03 +0300

    Update example LXC

--- 04-containers.tf
+++ 04-containers.tf
@@ -5,9 +5,14 @@ resource "proxmox_lxc" "example-container" {
 
   target_node     = local.MAIN_NODE
   ostemplate      = local.OS_TEMPLATE_PATH
+  password        = var.VM_USER_PASSWORD
   ssh_public_keys = <<-EOT
     ${var.SSH_PUBLIC_KEY}
   EOT
+
+  features {
+    nesting = true
+  }
 
   rootfs {
     storage = local.ROOTFS_STORAGE
