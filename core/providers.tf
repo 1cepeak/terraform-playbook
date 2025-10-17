31b2f27a98ded93d966429fbdd5fa6b305c62e6a
Nikita Usov <foxwel.info@gmail.com>
2025-10-17 06:35:49 +0300

    ci: update create config step

--- core/providers.tf
+++ core/providers.tf
@@ -23,7 +23,17 @@ terraform {
     }
   }
 
-  backend "s3" {}
+  backend "s3" {
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+    use_path_style              = true
+    insecure                    = true
+    bucket                      = "terraform"
+    key                         = "state/core/terraform.tfstate"
+    region                      = "eu-east-1"
+  }
 }
 
 provider "proxmox" {
